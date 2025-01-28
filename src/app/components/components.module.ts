1622530b4c1e25373a4980a0f1037fcdb2e378d7
Tu Nombre <tuemail@example.com>
2025-01-28 11:12:30 -0600

    Declaracion de variables

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,20 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
+import { IonicModule } from '@ionic/angular';
+import { CardPostComponent } from './card-post/card-post.component';
 
 
 
 @NgModule({
   declarations: [
-    LayoutComponent
+    LayoutComponent,
+    CardPostComponent,
   ],
   imports: [
+    IonicModule,
     CommonModule,
     
 
   ],
   exports:[
     LayoutComponent,
+    CardPostComponent,
   ]
 })
 export class ComponentsModule { }
