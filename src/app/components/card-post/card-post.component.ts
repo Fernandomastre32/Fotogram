7fd494da669b19652ac17febcf423eb27a62b4a9
Tu Nombre <tuemail@example.com>
2025-02-04 07:23:55 -0600

    Animacion de boton

--- src/app/components/card-post/card-post.component.ts
+++ src/app/components/card-post/card-post.component.ts
@@ -1,33 +1,24 @@
-import { Component, Input, input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-post',
   templateUrl: './card-post.component.html',
   styleUrls: ['./card-post.component.scss'],
-  standalone : false,
+  standalone: false,
 })
-export class CardPostComponent  implements OnInit {
-  //Header
- @Input() username: string = '';
- @Input() location: string = '';
- @Input() createAt: string = '';
- @Input() avatarURL: string = '';
-
- //Conter
- @Input() ContentUrl: string = '';
-
- //stats
- @Input() liked: boolean = false;
- @Input() saved: boolean = false;
+export class CardPostComponent implements OnInit {
+  @Input() username: string = "";
+  @Input() location: string = "";
+  @Input() avatarUrl: string = "";
+  @Input() imagenUrl: string = "";
+  @Input() contentUrl: string = "";
+  @Input() liked: boolean = false;
+  @Input() saved: boolean = false;
 
 
- //Info
-
- @Input() likes: number = 0;
- @Input() Description: string = '';
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
 }
